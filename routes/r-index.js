b0f124dbd7870f0f856843d32d2d97ad59260698
Priya sharma <priyasharma7128119@gmail.com>
2023-03-29 19:46:23 +0530

    "added authentication"

--- routes/r-index.js
+++ routes/r-index.js
@@ -3,3 +3,17 @@ const rToDo = require('./r-ToDo')
 module.exports = (app) => {
     app.use("/toDo", rToDo)
 }
+
+const passport = require('passport');
+module.exports = (app) => {
+    app.post('/login' ,
+    passport.authenticate('local' , { failureRedirect: '/login'}),
+    (req , res) => {
+        console.log("request body:" , req.body)
+        console.log("user details:" , req.user)
+        console.log("user authenticated:" , req.isAuthenticated())
+        return res.redirect('/toDo/');
+    }    );
+    app.use("/toDo" , rToDo)
+
+}
